c931d41a67e1b8844d5b7c1d9d9a5331226939b1
YasserB94 <yasserbal@icloud.com>
2022-11-30 11:41:45 +0100

    Base Input

--- src/components/form/BaseInput.vue
+++ src/components/form/BaseInput.vue
@@ -1,12 +1,57 @@
 <script setup>
-console.log("BaseInput works!")
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: "text",
+  },
+  placeholder: {
+    type: String,
+  },
+  label: {
+    type: String,
+  },
+  labelIsHidden: {
+    type: Boolean,
+    default: false,
+  },
+  stylesInput: {
+    type: [Array, String, Object],
+  },
+  stylesLabel: {
+    type: [Array, String, Object],
+  },
+  modelValue: {
+    type: [String, Number],
+  },
+});
+const emit = defineEmits(["update:modelValue"]);
+const updateValue = (e) => {
+  emit("update:modelValue", e.target.value);
+};
 </script>
-
 <template>
-$END$
-<p>BaseInput works!</p>
+  <label
+    v-show="!props.labelIsHidden"
+    :for="props.name"
+    :class="stylesLabel"
+    class="capitalize"
+    >{{ props.label || props.name }}</label
+  >
+  <input
+      v-bind="$attrs"
+    :name="props.name"
+    :type="props.type"
+    :class="[
+      { 'placeholder:capitalize': !props.placeholder },
+      props.stylesInput,
+    ]"
+    :placeholder="props.placeholder || `${props.name}...`"
+    @input="updateValue"
+  />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,27 @@
 <script setup>
-
+import { reactive } from "vue";
+import BaseInput from "../components/form/BaseInput.vue";
+let event = reactive({
+  title: "",
+  description:""
+});
+const logEvent = () => {
+  console.table(JSON.parse(JSON.stringify(event)));
+};
 </script>
 
 <template>
-  <main>
-    <h1 className="text-3xl font-bold underline">
-      Hello Tailwind!
-    </h1>
-  </main>
+  <section>
+    <header>
+      <h1>Vue 3 Forms</h1>
+    </header>
+    <main>
+      <form @submit.prevent="logEvent" class="flex flex-col">
+        <BaseInput  name="title" v-model="event.title" />
+        <BaseInput name="description" v-model="event.description" />
+      </form>
+    </main>
+    <footer>Hi Mom!</footer>
+  </section>
 </template>
+
