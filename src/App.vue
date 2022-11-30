a6ae107601aee44997e4aa5f727679f10685dec9
YasserB94 <yasserbal@icloud.com>
2022-11-30 10:07:32 +0100

    Added basic container

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,18 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <nav>
+  <div class="w-full">
+  <header class="flex justify-between p-4 space-x-60 lg:justify-center">
+    <h1>Hello Forms!</h1>
+    <nav class="space-x-4">
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
     </nav>
   </header>
-  <RouterView />
+  <main class="mx-2 max-w-xl md:mx-4 lg:mx-auto h-fit w-full">
+    <RouterView />
+  </main>
+  </div>
 </template>
 
 <style scoped></style>
