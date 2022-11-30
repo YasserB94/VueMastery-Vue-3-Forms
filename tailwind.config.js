4776df16abe0a4b4f474e7ca929adc8771c046ba
YasserB94 <yasserbal@icloud.com>
2022-11-30 10:00:01 +0100

    Added TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/*.vue", "./src/**/*.{js,ts,jsx,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
